13ca6e5a812bffd71667f5e5c5a0fe52e79f3bee
luan bernardes <luan.bernardes@transfeera.com>
2025-04-25 15:10:40 -0300

    feat(infra): throw error in axios http client

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -4,18 +4,19 @@ import axios, { AxiosResponse } from 'axios';
 export class AxiosHttpClient implements HttpClient {
   async request(data: HttpRequest): Promise<HttpResponse> {
     let axiosResponse: AxiosResponse;
+
     try {
       axiosResponse = await axios.request({
         url: data.url,
         method: data.method,
         data: data.body,
-        headers: data.headers
+        headers: data.headers,
+        validateStatus: (status) => status >= 200 && status <= 300
       });
     } catch (error: unknown) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      axiosResponse = error?.response;
+      throw new Error(error as string);
     }
+
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
