ceba8f979896d7e9eccdc6e38df534eb40387fbe
luan bernardes <luan.bernardes@transfeera.com>
2025-04-25 17:12:30 -0300

    feat: add pages in routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import HomePage from './pages/home/Home.page';
 import SignInPage from '@/pages/sign-in/Sign-in.page';
+import SignUpPage from '@/pages/sign-up/Sign-up.page';
+import WithAuth from '@/pages/auth/WithAuth.component';
 
 function App() {
   return (
@@ -10,8 +12,16 @@ function App() {
       <CssBaseline />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <WithAuth>
+              <HomePage />
+            </WithAuth>
+          }
+        />
         <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </>
   );
