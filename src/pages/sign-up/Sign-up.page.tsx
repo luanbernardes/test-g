592d7a48583e9abaaf69302d7a3aa8a4eba40d7f
luan bernardes <luan.bernardes@transfeera.com>
2025-04-25 11:04:18 -0300

    feat(SignIn): separate page and container

---
+++ src/pages/sign-up/Sign-up.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTemplate from '@/templates/main.template';
+
+const SignUpPage = () => {
+  return (
+    <MainTemplate>
+      <h1>Sign Up</h1>
+    </MainTemplate>
+  );
+};
+
+export default SignUpPage;
