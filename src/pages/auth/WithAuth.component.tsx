fc8639add04b33816ff92143716625f2225b3313
luan bernardes <luan.bernardes@transfeera.com>
2025-04-25 17:27:40 -0300

    feat(WithAuth):  create a component validate authentication

---
+++ src/pages/auth/WithAuth.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHasAuth } from '@/hooks/auth/hasAuth';
+import { useNavigate } from 'react-router-dom';
+import LoadingPage from '@/pages/loading/Loading.page';
+
+interface PropsWithAuth {
+  children: React.ReactNode;
+}
+
+const WithAuth: React.FC<PropsWithAuth> = ({ children }) => {
+  const { data, loading } = useHasAuth();
+  const navigate = useNavigate();
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  if (!loading && !data) {
+    navigate('/sign-up');
+    return <>not logged</>;
+  }
+
+  return <>{children}</>;
+};
+
+export default WithAuth;
