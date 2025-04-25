893a21db53aeee875d0369f04d336db1f8af9bb4
luan bernardes <luan.bernardes@transfeera.com>
2025-04-25 10:46:02 -0300

    feat: create base types of reqresAPI and .env with ReqresAPI URL

---
+++ src/@types/reqres.ts
@@ -0,0 +1,23 @@
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+export interface GetUsersResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+}
+
+export interface PostSignInBody {
+  email: string;
+  password: string;
+}
+export interface PostSignInResponse {
+  token: string;
+}
